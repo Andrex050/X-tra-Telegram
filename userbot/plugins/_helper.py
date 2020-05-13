171acdf8385b6694ef7f61e02758e7fcb818fcf5
znotash <65254885+znotash@users.noreply.github.com>
2020-05-13 21:13:45 +0200

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -28,7 +28,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " **non è un plugin valido!**")
         else:
-            help_string = """Userbot Helper di ash.. Provided by @zNotASH \n[Check out this dope af website](https://www.moddingunited.xyz/) \n
+            help_string = """Userbot Helper di ash.. __Provided by @zNotASH.__ \n
 `⬇️ Quì sotto troverai la lista dei plugin.`\n__ℹ️ Digita .help nome_plugin per avere info dettagliate su quel plugin.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
