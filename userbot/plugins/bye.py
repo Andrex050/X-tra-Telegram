a4f64f8b88473b1d048ad63daceebaa36e38ce95
znotash <65254885+znotash@users.noreply.github.com>
2020-05-13 22:00:46 +0200

    Update and rename bye1.py to bye.py

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -11,7 +11,7 @@ import time
 @borg.on(admin_cmd("bye", outgoing=True))
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("__🚀 Sto abbandonando il gruppo... See ya!__")
+        await e.edit("__💭 Sto abbandonando il gruppo... See ya!__")
         time.sleep(3)
         if '-' in str(e.chat_id):
             await borg(LeaveChannelRequest(e.chat_id))
